58bec28436f4beed28859b5ff7e67fb4645a40df
Nkosi Woseley <nwoseley14@gmail.com>
2016-09-01 23:32:21 +0000

    Moved promptUsername() call into 'request-users' callback, as acknowledgement

--- public/app/controllers/chatController.js
+++ public/app/controllers/chatController.js
@@ -1,6 +1,5 @@
 /* global angular, bootbox */
 
-
 angular.module('chatCtrl', ['SocketFactory'])
 
 .controller('chatController', function($scope, Socket) {
@@ -8,18 +7,21 @@ angular.module('chatCtrl', ['SocketFactory'])
     $scope.users = [];
     $scope.messages = [];
     // $scope.msg = '';
-    
+
     var promptUsername = function(message) {
         bootbox.prompt(message, function(name) {
             name = name || 'Guest' + new Date().getTime();
             if (name != null && (name = name.trim()).length > 0) {
-                Socket.emit('add-user', {username: name});
-            } else {
+                Socket.emit('add-user', {
+                    username: name
+                });
+            }
+            else {
                 promptUsername('You must enter a username.');
             }
         });
     };
-    
+
     $scope.sendMessage = function(msg) {
         if (msg != null && (msg = msg.trim()).length > 0) {
             Socket.emit('message', {
@@ -31,11 +33,11 @@ angular.module('chatCtrl', ['SocketFactory'])
     Socket.on('users', function(data) {
         $scope.users = data.users;
     });
-    
+
     Socket.on('message', function(data) {
         $scope.messages.push(data);
     });
-    
+
     Socket.on('add-user', function(data) {
         $scope.users.push(data.username);
         $scope.messages.push({
@@ -43,27 +45,26 @@ angular.module('chatCtrl', ['SocketFactory'])
             message: 'has entered the channel.'
         });
     });
-    
+
     Socket.on('remove-user', function(data) {
         if ($scope.users.indexOf(data.username) >= 0)
             $scope.users.splice($scope.users.indexOf(data.username), 1);
-        
+
         $scope.messages.push({
             username: data.username,
             message: 'has left the chat'
         });
     });
-    
+
     Socket.on('prompt-username', function(data) {
         promptUsername(data.message);
     });
-    
-    $scope.$on('$locationChangeStart', function(event){
+
+    $scope.$on('$locationChangeStart', function(event) {
         Socket.disconnect(true);
     });
-    
-    Socket.emit('request-users');
-    
-    promptUsername('What is your name?');
-    
+
+    Socket.emit('request-users', function() {
+        promptUsername('What is your name?');
+    });
 });
